84e31693d3c0312bdd567a2900c6a79303af95d1
zxck / waefur <datafetchsyserror@gmail.com>
2025-12-10 00:12:13 +0800

    prepare for publishing

--- dev.js
+++ dev.js
@@ -9,9 +9,9 @@ var balls = {}
 var ballsMotifs = {} // for leitmotifs that coalesce into a track
 let data = {}
 async function initiate() {
-    let rawJson = await fetch(window.location.origin + "/rhythm-doctor-leitmotifs.json");
+    let rawJson = await fetch(window.location.origin + "/rhythm-doctor-leitmotifs_dev.json");
     if (!rawJson.ok) {
-        rawJson = await fetch(window.location.origin + "/rhythm-doctor-motifs/rhythm-doctor-leitmotifs.json");
+        rawJson = await fetch(window.location.origin + "/rhythm-doctor-motifs/rhythm-doctor-leitmotifs_dev.json");
         console.log("Running on GitHub Pages!");
         if (!rawJson.ok)
             throw new Error(`Couldn't retrieve JSON! ${rawJson.status} - ${rawJson.statusText}`);
